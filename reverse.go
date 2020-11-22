2f925e9919b5dce7b86e1f025a183c7ce18fab33
Ado Pabianko <adopabianko@gmail.com>
2020-11-22 10:06:47 +0700

    add reverse

---
+++ reverse.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+
+	return
+}
+
+func main() {
+	str := "Katak"
+
+	strRev := reverse(str)
+	fmt.Println(str)
+	fmt.Println(strRev)
+}
